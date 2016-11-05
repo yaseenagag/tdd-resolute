2c549fb0e263d30544827d29089e48703e96abe0
Jared Grippe <jared@deadlyicon.com>
2016-11-04 17:00:15 -0700

    added test/server_test.js

--- test/server_test.js
+++ test/server_test.js
@@ -1,20 +1,258 @@
+const util = require('util');
+
+const BOOKS = require('./books.json')
+const GENRES = Object.keys(BOOKS.reduce((genres, book) => {
+  book.genres.forEach(genre => genres[genre] = true)
+  return genres
+}, {})).sort()
+
+
+const createBook = (attributes) => {
+  return request('post', '/api/books', attributes)
+    .then(response => {
+      if (response.status >= 400 && response.body && response.body.error){
+        throw response.body.error
+      }
+      expectResponseToHaveStatus(response, 201)
+      expect(response.headers['content-type']).to
+        .eql('application/json; charset=utf-8')
+      expect(response.body).to.be.an('object')
+      expect(response.body.id).to.be.a('number')
+      expect(response.body.title).to.eql(attributes.title)
+      expect(response.body.year).to.eql(attributes.year)
+      expect(response.body.author).to.eql(attributes.author)
+      expect(response.body.genres).to.eql((attributes.genres||[]).sort())
+      return response.body
+    })
+}
+
+const createBooks = (books) =>
+  Promise.all(books.map(createBook))
+
+const loadFixtureData = () => createBooks(BOOKS)
+
+const expectResponseToHaveStatus = (response, expectedStatus) => {
+  if (response.status === expectedStatus) return
+  const requestAsString = `request(${JSON.stringify(response.request.method)}, ${JSON.stringify(response.request.path)}, ${JSON.stringify(response.request.postBody, null, 2)})`.replace(/\n/g, "\n      ")
+  throw new Error(`
+    Expected the request:
+
+      ${requestAsString}
+
+    …to respond with status ${expectedStatus} but it responded with ${response.status}
+  `)
+}
+
 describe('HTTP Server', () => {
 
   describe('GET /ping', () => {
     it('should respond with "pong"', () => {
       return request('get', '/ping').then(response => {
-        expect(response).to.have.status(200)
+        expectResponseToHaveStatus(response, 200)
         expect(response.text).to.eql('pong')
       })
     })
   })
 
+  describe('POST /api/books', () => {
+    it('should create a book', () => {
+      return request('post', '/api/books', {
+        "title":"Starship Troopers",
+        "author":"Robert A. Heinlein",
+        "year": 2004,
+        "genres": ["Sci-fi", "Space", "Aliens"]
+      }).then(response => {
+        expectResponseToHaveStatus(response, 201)
+        expect(response.headers['content-type']).to
+          .eql('application/json; charset=utf-8')
+        const book = response.body
+        expect(book.title).to.eql("Starship Troopers")
+        expect(book.author).to.eql("Robert A. Heinlein")
+        expect(book.year).to.eql(2004)
+        expect(book.genres).to.eql(["Sci-fi", "Space", "Aliens"].sort())
+      })
+    })
+    context('when missing title', () => {
+      it('should render 400 bad request', () => {
+        return request('post', '/api/books', {
+          "author":"Robert A. Heinlein",
+          "year": 2004
+        }).then(response => {
+          expectResponseToHaveStatus(response, 400)
+          expect(response.headers['content-type']).to
+            .eql('application/json; charset=utf-8')
+          expect(response.body).to.have.any.key('error')
+          expect(response.body.error).to.have.any.key('message')
+          expect(response.body.error.message).to.eql('title cannot be blank')
+        })
+      })
+    })
+  })
 
-  // GET /api/books
-  // GET /api/books?page=2
-  // GET /api/books/:bookId
-  // POST /api/books/:bookId
-  // POST /api/books/:bookId/delete
-  // GET /api/books/search?title&author&genre
+  context('with fixture data', () => {
+    let fixtures
+
+    const findFixtureForBook = (book) =>
+      fixtures.find(fixture => fixture.id === book.id)
+
+    beforeEach(() =>
+      loadFixtureData().then(books =>
+        fixtures = books
+      )
+    )
+
+    describe('GET /api/books', () => {
+      it('should render 10 books', () => {
+        return request('get', '/api/books').then(response => {
+          expectResponseToHaveStatus(response, 200)
+          expect(response.headers['content-type']).to
+            .eql('application/json; charset=utf-8')
+          const books = response.body
+          expect(books.length).to.eql(10)
+          books.forEach(book => {
+            const fixture = findFixtureForBook(book)
+            expect(fixture).to.eql(book)
+          })
+        })
+      })
+    })
+
+    describe('GET /api/books?page=2', () => {
+      it('should render the next 10 books', () => {
+        return Promise.all([
+          request('get', '/api/books'),
+          request('get', '/api/books?page=2'),
+        ]).then(([page1Request, page2Request]) => {
+          const page1Books = page1Request.body
+          const page2Books = page2Request.body
+          expect(page1Books.length).to.eql(10)
+          expect(page2Books.length).to.eql(10)
+
+          const page1BookIds = page1Books.map(book => book.id)
+          const page2BookIds = page2Books.map(book => book.id)
+          page1BookIds.forEach(bookId => {
+            expect(page2BookIds).to.not.include(bookId)
+          })
+          page2BookIds.forEach(bookId => {
+            expect(page1BookIds).to.not.include(bookId)
+          })
+        })
+      })
+    })
+
+    describe('GET /api/authors', () => {
+      it('should render 10 authors', () => {
+        return request('get', '/api/authors').then(response => {
+          expectResponseToHaveStatus(response, 200)
+          expect(response.headers['content-type']).to
+            .eql('application/json; charset=utf-8')
+          const authors = response.body
+          expect(authors.length).to.eql(10)
+        })
+      })
+    })
+
+    describe('GET /api/authors?page=2', () => {
+      it('should render the next 10 authors', () => {
+        return Promise.all([
+          request('get', '/api/authors'),
+          request('get', '/api/authors?page=2'),
+        ]).then(([page1Request, page2Request]) => {
+          const page1Authors = page1Request.body
+          const page2Authors = page2Request.body
+          expect(page1Authors.length).to.eql(10)
+          expect(page2Authors.length).to.eql(10)
+
+          const page1AuthorIds = page1Authors.map(author => author.id)
+          const page2AuthorIds = page2Authors.map(author => author.id)
+          page1AuthorIds.forEach(authorId => {
+            expect(page2AuthorIds).to.not.include(authorId)
+          })
+          page2AuthorIds.forEach(authorId => {
+            expect(page1AuthorIds).to.not.include(authorId)
+          })
+        })
+      })
+    })
+
+    describe('GET /api/books/12', () => {
+      context('when the book exists', () => {
+        it('should render book 12', () => {
+          return request('get', '/api/books/12').then(response => {
+            expectResponseToHaveStatus(response, 200)
+            expect(response.headers['content-type']).to
+              .eql('application/json; charset=utf-8')
+            const book = response.body
+            expect(book.id).to.eql(12)
+            const fixture = findFixtureForBook(book)
+            expect(fixture).to.eql(book)
+          })
+        })
+      })
+      context('when the book doesn\'t exist', () => {
+        it('should render nothing with status 404', () => {
+          return request('get', '/api/books/98989898').then(response => {
+            expectResponseToHaveStatus(response, 404)
+            expect(response.headers['content-type']).to
+              .eql('application/json')
+            expect(response.body).to.eql('')
+          })
+        })
+      })
+    })
+
+    describe('GET /api/genres', () => {
+      it('should render up to 10 genres', () => {
+        return request('get', '/api/genres').then(response => {
+          expectResponseToHaveStatus(response, 200)
+          expect(response.headers['content-type']).to
+            .eql('application/json; charset=utf-8')
+          const genres = response.body
+          expect(genres.length).to.eql(10)
+          const genreNames = genres.map(genre => genre.name).sort()
+          expect(genreNames).to.eql(GENRES.slice(0,10))
+        })
+      })
+    })
+
+    describe('POST /api/book/12', () => {
+      it('should update the book', () => {
+        return request('post', '/api/books/12', {
+          "title":"Starship Troopers",
+          "author":"Robert A. Heinlein",
+          "year": 2004,
+        }).then(response => {
+          expectResponseToHaveStatus(response, 200)
+          expect(response.headers['content-type']).to
+            .eql('application/json; charset=utf-8')
+          const book = response.body
+          expect(book.title).to.eql("Starship Troopers")
+          expect(book.author).to.eql("Robert A. Heinlein")
+          expect(book.year).to.eql(2004)
+        })
+      })
+    })
+
+    describe('POST /api/book/12/delete', () => {
+      it('should update delete the book', () => {
+        return request('get', '/api/books/12')
+          .then(response => {
+            expectResponseToHaveStatus(response, 200)
+          })
+          .then(() => request('post', '/api/books/12/delete'))
+          .then(response => {
+            expectResponseToHaveStatus(response, 200)
+            expect(response.headers['content-type']).to
+              .eql('application/json')
+            expect(response.body).to.eql('')
+          })
+          .then(() => request('get', '/api/books/12'))
+          .then(response => {
+            expectResponseToHaveStatus(response, 404)
+          })
+      })
+    })
+
+  })
 
 })
--- test/setup.js
+++ test/setup.js
@@ -7,27 +7,49 @@ global.chaiHttp = require('chai-http')
 global.server = require('../server')
 
 chai.use(chaiHttp)
+chai.config.includeStack = true
 
 global.browserInstance
 
 beforeEach(() => {
   global.browserInstance = chai.request.agent(server)
+  /*
+   * This request needs to empty out and migrate up your database
+   */
+  return request('post', '/api/test/reset-db')
+    .then(response => {
+      if (response.status !== 200) throw new Error(`
+
+Failed to reset your database!
+
+Please ensure you app responds to:
+  POST /api/test/reset-db
+
+This endpoint should truncate your entire database as well as migrate it up to your latest schema.
+
+Note: This request is sent before every single mocha test via a \`beforeEach\` in /test/setup.js
+      `)
+    })
 })
 
 // request('GET', '/api/users/12').then(response)
-global.request = (method, url, postBody) => {
+global.request = (method, path, postBody) => {
   method = method.toLowerCase()
   return new Promise((resolve, reject) => {
-    var req = browserInstance[method](url)
+    var req = browserInstance[method](path)
     if (method === 'post' && postBody) req = req.send(postBody)
     req.end((error, response) => {
       if (error && error.status >= 500) {
-        console.warn(chalk.red('Server Error: '+response.body.error.message))
-        console.warn(chalk.red(response.body.error.stack))
+        console.warn('Server Error: '+response.body.error.message)
+        console.warn(response.body.error.stack)
         reject(error)
       }else{
+        response.request = {
+          method, path, postBody
+        }
         resolve(response)
       }
     })
   })
 }
+
